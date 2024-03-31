3b9f0e61d9773c5b53bfe796f7def60475cbc774
Miraz <w3miraj@gmail.com>
2024-03-31 13:26:39 +0600

    project

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./menu.css"
-import {Navbar,Container,Nav} from 'react-bootstrap';
+import {Navbar,Container,Nav, NavLink} from 'react-bootstrap';
 import logo from "../../assets/logo.png"
 import { Link } from 'react-router-dom';
 
@@ -9,20 +9,20 @@ const Menu = () => {
     <>
      <Navbar expand="lg" className='menu_bg'>
       <Container>
-        <Link href={"/"}><img src={logo} alt="logo" /></Link>
+        <Link to={"/"}><img src={logo} alt="logo" /></Link>
         <Navbar.Brand href="#home"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto menu_nav">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/About_us"}>About us</Link>
-            <Link href={"/Features"}>Features</Link>
-            <Link href={"/Pricing"}>Pricing</Link>
-            <Link href={"/FAQ"}>FAQ</Link>
-            <Link href={"/Blog"}>Blog</Link>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/About_us"}>About us</Link>
+            <Link to={"/Features"}>Features</Link>
+            <Link to={"/Pricing"}>Pricing</Link>
+            <Link to={"/FAQ"}>FAQ</Link>
+            <Link to={"/Blog"}>Blog</Link>
           </Nav>
         <div className="contact">
-            <Link href={"/Contact"}>Contact</Link>
+            <Link to={"/Contact"}>Contact</Link>
         </div>
         </Navbar.Collapse>
       </Container>
