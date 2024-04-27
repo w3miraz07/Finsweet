f4c4fb7a9f030dbdee110289ef025121f38d618d
Miraz <w3miraj@gmail.com>
2024-04-27 23:12:50 +0600

    p

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -17,7 +17,7 @@ const Menu = () => {
             <a href=""></a>
             <Link href="#/" to={"/"}>Home</Link>
             <Link href="#/About_us" to={"/About_us"} refresh="true">About us</Link>
-            <Link href="#/Features" to={"/Features"} refresh="true">Features</Link>
+            <Link href="https://darling-brigadeiros-06e81a.netlify.app/Features" to={"/Features"} refresh="true">Features</Link>
             <Link href="#/Pricing" to={"/Pricing"} refresh="true">Pricing</Link>
             <Link href="#/FAQ" to={"/FAQ"} refresh="true">FAQ</Link>
             <Link href="#/Blog" to={"/Blog"} refresh="true">Blog</Link>
