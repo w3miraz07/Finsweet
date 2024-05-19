761f2922e28657d8884439330d775ab28871eb3f
Miraz <w3miraj@gmail.com>
2024-05-19 21:25:06 +0600

    demo

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -15,12 +15,12 @@ const Menu = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto menu_nav">
             <a href=""></a>
-            <Link href="#/" to={"/"}>Home</Link>
-            <Link href="#/About_us" to={"/About_us"} refresh="true">About us</Link>
-            <Link href="#/Features" to={"/Features"} refresh="true">Features</Link>
-            <Link href="#/Pricing" to={"/Pricing"} refresh="true">Pricing</Link>
-            <Link href="#/FAQ" to={"/FAQ"} refresh="true">FAQ</Link>
-            <Link href="#/Blog" to={"/Blog"} refresh="true">Blog</Link>
+            <Link href="/" to={"/"}>Home</Link>
+            <Link href="/About_us" to={"/About_us"} refresh="true">About us</Link>
+            <Link href="/Features" to={"/Features"} refresh="true">Features</Link>
+            <Link href="/Pricing" to={"/Pricing"} refresh="true">Pricing</Link>
+            <Link href="/FAQ" to={"/FAQ"} refresh="true">FAQ</Link>
+            <Link href="/Blog" to={"/Blog"} refresh="true">Blog</Link>
           </Nav>
         <div className="contact">
             <Link to={"/Contact"} refresh="true">Contact</Link>
--- src/components/root/RootLayout.jsx
+++ src/components/root/RootLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Menu from '../menu/Menu'
 import { Outlet } from 'react-router-dom'
 import Footer from '../footer/Footer'
+import { ScrollRestoration } from "react-router-dom";
 
 const RootLayout = () => {
   return (
     <>
+    <ScrollRestoration/>
     <Menu/>
     <Outlet/>
     <Footer/>
